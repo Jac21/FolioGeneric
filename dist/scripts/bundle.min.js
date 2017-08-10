a592dfc3f8bb53f0b02f178c454459981efaeb8e
Jeremy Cantu <jcantu521@gmail.com>
2017-08-10 15:57:09 -0500

    Updated packages, small fixes

--- dist/scripts/bundle.min.js
+++ dist/scripts/bundle.min.js
@@ -5,35 +5,40 @@
 	angular.module('folioGenericApp', ['ui.router', 'responsive-images'])
 		.config(function($stateProvider, $urlRouterProvider, $compileProvider, $locationProvider) {
 
-		  $compileProvider.debugInfoEnabled(false);
+			$compileProvider.debugInfoEnabled(false);
 
-		  $stateProvider
-		  .state('developer', {
-		    url: '/developer',
-		    templateUrl: 'templates/main/developer.html',
-		    controller: 'DeveloperController'
-		  })
-		  .state('photographer', {
-		    url: '/photographer',
-		    templateUrl: 'templates/main/photographer.html',
-		    controller: 'PhotographerController'
-		  })
-		  .state('demo-photoshoot', {
-		    url: '/demo-photoshoot',
-		    templateUrl: 'templates/photoshoots/demo-photoshoot.html',
-		    controller: 'PhotographerController'
-		  })
-		  .state('demo-project', {
-		    url: '/demo-project',
-		    templateUrl: 'templates/projects/demo-project.html',
-		    controller: 'DeveloperController'
-		  })
-		  .state('demo-project-two', {
-		    url: '/demo-project-two',
-		    templateUrl: 'templates/projects/demo-project-two.html',
-		    controller: 'DeveloperController'
-		  });
+			$stateProvider
+			.state('developer', {
+				url: '/developer',
+				templateUrl: 'templates/main/developer.html',
+				controller: 'DeveloperController'
+			})
+			.state('photographer', {
+				url: '/photographer',
+				templateUrl: 'templates/main/photographer.html',
+				controller: 'PhotographerController'
+			})
+			.state('demo-photoshoot', {
+				url: '/demo-photoshoot',
+				templateUrl: 'templates/photoshoots/demo-photoshoot.html',
+				controller: 'PhotographerController'
+			})
+			.state('demo-project', {
+				url: '/demo-project',
+				templateUrl: 'templates/projects/demo-project.html',
+				controller: 'DeveloperController'
+			})
+			.state('demo-project-two', {
+				url: '/demo-project-two',
+				templateUrl: 'templates/projects/demo-project-two.html',
+				controller: 'DeveloperController'
+			});
 
-		  $urlRouterProvider.otherwise('/developer');
+			$urlRouterProvider.otherwise('/developer');
+
+			$locationProvider.html5Mode({
+				enabled: true,
+				requireBase: true
+			});
 		});
 })();
--- scripts/controllers/PhotographerController.js
+++ scripts/controllers/PhotographerController.js
@@ -8,26 +8,26 @@
 
 				// image data array
 				$scope.images = [
-				   {
-				      "caption":"Demo Photo",
-				      "url":"dist/images/photographer/photos/demo-photo-one.jpg",
-				      "alt":"Demo Photo One"
-				   },
-				   {
-				      "caption":"Demo Photo",
-				      "url":"dist/images/photographer/photos/demo-photo-two.jpg",
-				      "alt":"Demo Photo Two"
-				   },
-				   {
-				      "caption":"Demo Photo",
-				      "url":"dist/images/photographer/photos/demo-photo-three.jpeg",
-				      "alt":"Demo Photo Three"
-				   },
-				   {
-				      "caption":"Demo Photo",
-				      "url":"dist/images/photographer/photos/demo-photo-four.jpeg",
-				      "alt":"Demo Photo Four"
-				   }
+					{
+						"caption":"Demo Photo",
+						"url":"dist/images/photographer/photos/demo-photo-one.jpg",
+						"alt":"Demo Photo One"
+					},
+					{
+						"caption":"Demo Photo",
+						"url":"dist/images/photographer/photos/demo-photo-two.jpg",
+						"alt":"Demo Photo Two"
+					},
+					{
+						"caption":"Demo Photo",
+						"url":"dist/images/photographer/photos/demo-photo-three.jpeg",
+						"alt":"Demo Photo Three"
+					},
+					{
+						"caption":"Demo Photo",
+						"url":"dist/images/photographer/photos/demo-photo-four.jpeg",
+						"alt":"Demo Photo Four"
+					}
 				];
 				
 				// utility function for linking between views
@@ -35,8 +35,7 @@
 				  $location.path( path );
 				};
 
-				// utility function to set viewport to the top of the page 
-				// on element click
+				// utility function to set viewport to the top of the page on element click
 				$scope.backToTop = function() {
 					$anchorScroll();
 				};
