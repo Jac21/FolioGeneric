450d7912fd7c79fc63d2cb003eb2b207fce662b2
Jeremy Cantu <jcantu521@gmail.com>
2018-04-20 16:00:27 -0500

    Formatting

--- scripts/controllers/DeveloperController.js
+++ scripts/controllers/DeveloperController.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   "use strict";
 
   angular // eslint-disable-line no-undef
@@ -7,12 +7,12 @@
       "$scope",
       "$location",
       "$anchorScroll",
-      function ($scope, $location, $anchorScroll) {
+      function($scope, $location, $anchorScroll) {
         // set page class
         $scope.pageClass = "developer";
 
         // utility function for linking between views
-        $scope.go = function (path) {
+        $scope.go = function(path) {
           $location.path(path);
         };
 
@@ -37,13 +37,13 @@
         };
 
         // utility function to add active css class to target element
-        $scope.activate = function (event) {
+        $scope.activate = function(event) {
           $(event.target).addClass("active"); // eslint-disable-line no-undef
         };
 
         // utility function to set viewport to the top of the page
         // on element click
-        $scope.backToTop = function () {
+        $scope.backToTop = function() {
           $anchorScroll();
         };
 
@@ -56,12 +56,13 @@
         });
 
         // initialize blog container wrapper style
-        var secondaryThemeHexCode = '#ffab40';
+        var secondaryThemeHexCode = "#ffab40";
 
-        if (document.querySelector('div.container.blog-container')) {
-          document.body.parentNode.style.boxShadow = "0 0 0 5px " + secondaryThemeHexCode + " inset";
+        if (document.querySelector("div.container.blog-container")) {
+          document.body.parentNode.style.boxShadow =
+            "0 0 0 5px " + secondaryThemeHexCode + " inset";
         } else {
-          document.body.parentNode.style.boxShadow = '';
+          document.body.parentNode.style.boxShadow = "";
         }
       }
     ]);
